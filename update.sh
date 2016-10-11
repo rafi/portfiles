17a9d12c0e7d47216bf85f6d19dfcdba4bcb5cb1
Rafael Bodill <justrafi@gmail.com>
2016-10-11 19:27:25 +0300

    Improve ports update & upgrade-check script

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 for dir in */*; do
-	port upgrade "${dir##*/}" && port install "${dir##*/}"
-	port livecheck "${dir##*/}"
+	port="${dir##*/}"
+	echo ":: ${port}"
+	port upgrade "${port}" && port install "${port}"
+	port livecheck "${port}"
 done
